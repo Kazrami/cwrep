49c1fe0173a5bd6cf03ff845485814c24be0bd67
Kazrami <72901491+Kazrami@users.noreply.github.com>
2020-11-01 18:01:47 +0700

    Update program.c

--- src/program.c
+++ src/program.c
@@ -9,18 +9,16 @@ int main() {
   while (step != 0x1B) {
     puts("1) Addition");
     puts("2) Subtraction");
-    puts("3) Multiplication");
-    puts("4) Division");
-    puts("5) Exponentiation");
+    
     
     puts("Selected action:");
     scanf("%d", &step);
 
-    if (step < 0 || step > 5) {
+    if (step < 0 || step > 2) {
       exit(1);
     }
 
-    if (step > 0 || step < 5) {
+    if (step > 0 || step < 2) {
       switch (step) {
       case 1:
         puts("Input a and b");
@@ -36,26 +34,7 @@ int main() {
         printf("%4.4lf\n", result);
         break;
 
-      case 3:
-        puts("Input a and b");
-        scanf("%lf %lf", &a, &b);
-        result = multiplication(a, b);
-        printf("%4.4lf\n", result);
-        break;
-
-      case 4:
-        puts("Input a and b");
-        scanf("%lf %lf", &a, &b);
-        result = division(a, b);
-        printf("%4.4lf\n", result);
-        break;
-
-      case 5:
-        puts("Input a and b");
-        scanf("%lf %lf", &a, &b);
-        result = function_pow(a, b);
-        printf("%4.4lf\n", result);
-        break;
+      
       }
     }
   }
