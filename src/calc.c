874ffbcb16d030412f72cdcfbe44a9a192a1b7aa
Kazrami <72901491+Kazrami@users.noreply.github.com>
2020-11-01 18:00:44 +0700

    Update calc.c

--- src/calc.c
+++ src/calc.c
@@ -8,23 +8,5 @@ double minus(double a, double b) {
   return a - b;
 }
 
-double multiplication(double a, double b) {
-  return a * b;
-}
-
-double division(double a, double b) {
-  return a / b;
-}
-
-double function_pow(double a, int b) {
-  double result = 1;
-  if (b == 0) {
-    result = 1;
-    return result;
-  }
-
-  for (int i = 0; i < b; i++) {
-    result = a * result;
-  }
   return result;
 }
